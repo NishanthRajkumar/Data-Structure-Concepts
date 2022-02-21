bc05ab2d3bb8fbcacd4070f8f00627f2b759a3c3
Nishanth <rajkumarnishanth@gmail.com>
2022-02-21 11:27:42 +0530

    Simple append to linkedlist

--- CustomDataStructure/Program.cs
+++ CustomDataStructure/Program.cs
@@ -1,2 +1,15 @@
-﻿Console.Title = "My Data Structure";
+﻿using MyDataStructureLibrary;
+
+Console.Title = "My Data Structure";
 Console.WriteLine("==========Custom Data Structure==========");
+
+UnOrderedLinkedList<int> list = new UnOrderedLinkedList<int>();
+
+Console.WriteLine("Adding 56, 30, 70 to list");
+list.Append(56);
+list.Append(30);
+list.Append(70);
+
+
+Console.WriteLine("List 1");
+list.Display();
--- MyDataStructureLibrary/UnOrderedLinkedList.cs
+++ MyDataStructureLibrary/UnOrderedLinkedList.cs
@@ -1,7 +1,45 @@
-﻿namespace MyDataStructureLibrary
-{
-    public class Class1
-    {
+﻿namespace MyDataStructureLibrary;
 
+public class UnOrderedLinkedList<T> where T : IComparable<T>
+{
+    public Node<T> head;
+
+    public UnOrderedLinkedList()
+    {
+        head = null;
+    }
+
+    /// <summary>
+    /// Creates and returns empty list.
+    /// </summary>
+    /// <returns>empty UnOrderedLinkedList object</returns>
+    public UnOrderedLinkedList<T> List()
+    {
+        UnOrderedLinkedList<T> emptyList = new UnOrderedLinkedList<T>();
+        return emptyList;
+    }
+
+    public void Append(T data)
+    {
+        Node<T> node = new Node<T>(data);
+        if (head == null)
+            head = node;
+        else
+        {
+            Node<T> temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+            temp.next = node;
+        }
+    }
+
+    public void Display()
+    {
+        Node<T> temp = head;
+        while (temp != null)
+        {
+            Console.Write("\n" + temp.data);
+            temp = temp.next;
+        }
     }
 }
