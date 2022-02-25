5471c39509c2728c9295fa534cba735b25a7849a
Nishanth <rajkumarnishanth@gmail.com>
2022-02-25 22:11:08 +0530

    add data to binary search tree
    BinaryNode is the class for BST nodes
    BinarySearchTree handles BinaryNode to make tree

--- CustomDataStructure/Program.cs
+++ CustomDataStructure/Program.cs
@@ -1,14 +1,12 @@
-﻿using CustomDataStructure;
+﻿using MyDataStructureLibrary;
 
 Console.Title = "My Data Structure";
 Console.WriteLine("==========Custom Data Structure==========");
 
-// String used to test the word frequency count of the para
-string testString;
-testString = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+BinarySearchTree<int> intTree = new BinarySearchTree<int>();
 
-StringOperation wordOperation = new(testString);
+intTree.Add(56);
+intTree.Add(30);
+intTree.Add(70);
 
-Console.WriteLine($"Frequency of 'Paranoid': {wordOperation.GetVal("paranoid").Count}");
-
-Console.WriteLine(wordOperation.GetAvoidedString());
+intTree.Display();
